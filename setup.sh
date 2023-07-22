464a5ae536e250263a337ea20c732eda14f126a0
Ali Nikseresht <ali.nikseresht21@gmail.com>
2023-07-22 15:17:51 -0400

    add git config

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 ln -s $SCRIPTPATH/vim/.vimrc ~/.vimrc
 ln -s $SCRIPTPATH/zsh/.zshrc ~/.zshrc
+ln -s $SCRIPTPATH/git/.gitconfig ~/.gitconfig
 
 source ~/.zshrc
