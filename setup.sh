70374b9f8276644365bb48bae59a76bb1bb3fc3d
Ali Nikseresht <ali.nikseresht21@gmail.com>
2024-08-06 20:27:58 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
-#! /bin/zsh
+#
+ /bin/zsh
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 
 ln -nfs $SCRIPTPATH/vim ~/.vim
 ln -nfs $SCRIPTPATH/zsh/.zshrc ~/.zshrc
 ln -nfs $SCRIPTPATH/git/.gitconfig ~/.gitconfig
 
 zsh $SCRIPTPATH/zsh/install_plugins.sh
+
+git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 source ~/.zshrc
